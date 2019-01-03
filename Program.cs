5159751a8948098b739a68e42913bfef635ec8fd
Bob Tabor <bobtabor@outlook.com>
2019-01-03 14:21:05 -0600

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleClassTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The Console class provides methods to interact
+            //  with a user via the Console Window / Command Line.
+            //  We'll be using it A LOT!
+
+            // Obviously, this prints a literal string
+            // and then a linefeed.
+            // Console.WriteLine("Hello World!");
+
+            // This also prints a literal string,  
+            // but no linefeed.
+            // Console.Write("Hello ");
+            // Console.Write("World!");
+            // Console.WriteLine();
+
+            // We'll use this to capture user input
+            // ... or to pause the execution of the app
+            // waiting for the user to press the enter key.
+            // Console.ReadLine();
+
+            // Console.ForegroundColor = ConsoleColor.Blue;
+            // Console.WriteLine("Hello World!");
+            // Console.ResetColor();
+
+            // Console.BackgroundColor = ConsoleColor.Red;
+            // Console.WriteLine("Hello World!");
+            // Console.ResetColor();
+
+            // It beeps!
+            // Console.Beep();
+
+            // You can clear the screen!
+            // Console.Clear();
+        }
+    }
+}
